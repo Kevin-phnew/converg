834f46a1fc1e83c919e0122b7f35ed879b62c337
Kevin-phnew <123456Peng>
2020-03-05 14:49:32 +0800

    postgre

--- src/main/java/schema/ConvergDB.java
+++ src/main/java/schema/ConvergDB.java
@@ -1,5 +1,7 @@
 package schema;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import common.DataSource;
 import common.JdbcService;
 import common.JdbcServiceFactory;
@@ -13,7 +15,7 @@ import java.util.Scanner;
  * @Date 2020/3/4
  */
 public class ConvergDB {
-    public static void main(String args[]){
+    public static void main(String[] args) {
 
         System.out.println( "===============================\n" +
                             "ConvergDB JDBC schema extractor\n" +
@@ -28,30 +30,47 @@ public class ConvergDB {
 
     /**
      * 该方法执行数据库连接、元数据获取、schema文件输出
+     * java -jar convergdb-schema-extractor.jar \
+     * schema=clickstream table=clicks \
+     * database=jdbc:postgresql//somehostname:5432/web_analytics \
+     * db_engine=postgres \
+     * driver_jar=/jars/postgres9_jdbc.jar \
+     * driver_class=postgresql.jdbc.Driver
      */
     private static void convergDBAction(){
-        String userName = System.getProperty("table");
-        String passwd = System.getProperty("table");
-        String jdbcUrl = System.getProperty("database");
-        String jdbcType = System.getProperty("db_engine");
-        String info = "Attempting connection to "+jdbcUrl +"as analytics_user...";
-        System.out.println(info);
+        String userName = System.getProperty("userName");
+        String passwd = System.getProperty("passwd");
+        String database = System.getProperty("database");
+        String engine = System.getProperty("db_engine");
+        String schema = System.getProperty("schema");
+        String table = System.getProperty("table");
 
-        /**
-         * 根据不同数据库信息参数设置
-         */
-        String jdbcUser = "root";
-        String jdbcPassword = "1234";
-//        String dbName = "";
-        String tvname = "emp";
-
-        /**
-         * 元数据抽取部分
-         */
-        DataSource param = new DataSource(jdbcType, jdbcUrl, jdbcUser, jdbcPassword, tvname);
+        DataSource param = new DataSource(engine, database, userName, passwd, "", schema, table);
         JdbcService jdbcService = JdbcServiceFactory.getJdbcService(param);
-        List<Map<String, Object>> fields = jdbcService.getTableColumnsAndType(tvname);
-        fields.stream().forEach(e -> System.out.println(e));
+        List<Map<String, Object>> fields = jdbcService.getTableColumnsAndType();
+        String json = ((JSONArray) JSONObject.toJSON(fields)).toJSONString();
+        System.out.println(json);
+//        String info = "Attempting connection to "+database +"as analytics_user...";
+//        System.out.println(info);
+//
+//        //创建连接
+//        Connection conn = null;
+//        conn = new JDBCUtil().getConn(database,engine);
+//        if(null == conn){
+//            System.out.println("\n" +
+//                    "Sorry, your connection is wrong. Please check the connection parameters");
+//            return;
+//        }
+//        System.out.println("Connected!");
+//        System.out.println("Extracting schema for the following objects:");
+//
+//        JSONObject json = null;
+//        //抽取元数据
+////      json = new GetMetaDataImp().getMetaData("","");
+//        if(json == null){
+//            System.out.println("Extracting schema failed!");
+//            return;
+//        }
 
         System.out.println("Schema extracted successfully!");
 
@@ -71,10 +90,7 @@ public class ConvergDB {
 //        }catch (){
 //
 //        }
-        /**
-         * DDL到Schema
-         */
-        extraSchema.exportSchema(fields);
+//        extraSchema.exportSchema(json);
 
         System.out.println("Process complete");
 
