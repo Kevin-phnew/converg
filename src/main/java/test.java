73231257c78dd968481dd333919dcb66611c0898
Kevin-phnew <123456Peng>
2020-03-06 14:23:03 +0800

    添加junit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,23 +2,14 @@ import common.Column;
 import common.DataSource;
 import common.JdbcService;
 import common.JdbcServiceFactory;
+import org.junit.Test;
 
 import java.util.List;
 
 public class test {
-    public static void main(String[] args) {
-//        String jdbcType = "mysql";
-//        String jdbcUrl = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
-//        String jdbcUser = "root";
-//        String jdbcPassword = "1234";
-//        String dbName = "";
-//        String schema = "";
-//        String tvName = "emp";
-//        DataSource param = new DataSource(jdbcType, jdbcUrl, jdbcUser, jdbcPassword, dbName, schema, tvName);
-//        JdbcService jdbcService = JdbcServiceFactory.getJdbcService(param);
-//        List<Column> fields = jdbcService.getTableColumnsAndType();
-//        fields.stream().forEach(e -> System.out.println(JSONObject.toJSON(e)));
 
+    @Test
+    public void postgresSql(){
         String jdbcType = "postgres";
         String jdbcUrl = "jdbc:postgresql://localhost:5432/test";
         String jdbcUser = "postgres";
@@ -26,9 +17,26 @@ public class test {
         String dbName = "test";
         String schema = "pg_catalog";
         String tvName = "pg_user";
+
+        getData(jdbcType,jdbcUrl,jdbcUser,jdbcPassword,dbName,schema,tvName);
+    }
+
+    @Test
+    public void oracleSql(){
+        System.out.println("hello oracle");
+    }
+
+    @Test
+    public void mySql(){
+
+    }
+
+    public void getData(String jdbcType, String jdbcUrl, String jdbcUser, String jdbcPassword, String dbName, String schema, String tvName){
         DataSource param = new DataSource(jdbcType, jdbcUrl, jdbcUser, jdbcPassword, dbName, schema, tvName);
         JdbcService jdbcService = JdbcServiceFactory.getJdbcService(param);
         List<Column> fields = jdbcService.getTableColumnsAndType();
         fields.stream().forEach(e -> System.out.println(e.toString()));
     }
+
+
 }
