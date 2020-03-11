8662335d5298188af507d1c1749ee3d22bc7c592
Kevin-phnew <123456Peng>
2020-03-11 12:28:41 +0800

    update class name

--- src/main/java/metadata/ANSIMetaData.java
+++ src/main/java/metadata/ANSIMetaData.java
@@ -4,8 +4,6 @@ import common.JdbcService;
 import common.JdbcServiceFactory;
 import model.Column;
 import model.DataSource;
-import org.apache.commons.lang3.StringUtils;
-import util.StringUtil;
 
 import java.util.List;
 
@@ -34,46 +32,8 @@ public class ANSIMetaData {
             return null;
         List<Column> fields = jdbcService.getTableColumnsAndType();
         System.out.println("Schema extracted successfully!");
-//        String json = ((JSONArray) JSONObject.toJSON(fields)).toJSONString();
-//        System.out.println(json);
         return fields;
     }
 
-    /**
-     * @return List<Column>
-     * int(n) n<8	interger
-     * int(n) n>=8	bigint
-     * timestamp(n)	timestamp
-     * timestamp(n) with time zone	timestamptz
-     * time(n)	time
-     * time(n) with time zone	time with time zone
-     */
-    public static List<Column> changeANSIToConvergeMeta(List<Column> columns) {
-//        List<Column> columns = getANSIMetaData();
-        for (Column e : columns) {
-            String columnType = e.getColumnType();
-            if (columnType.startsWith("int")) {
-                String n = StringUtil.getNumberFromText(columnType);
-                if (StringUtils.isBlank(n) || Integer.parseInt(n) < 8) {
-                    e.setColumnType("interger");
-                } else {
-                    e.setColumnType("bigint");
-                }
-            } else if (columnType.startsWith("timestamp")) {
-                if (columnType.endsWith("zone")) {
-                    e.setColumnType("timestamptz");
-                } else {
-                    e.setColumnType("timestamp");
-                }
-            } else if (columnType.startsWith("time")) {
-                if (columnType.endsWith("zone")) {
-                    e.setColumnType("time with time zone");
-                } else {
-                    e.setColumnType("time");
-                }
-            }
-        }
-        return columns;
-    }
 
 }
