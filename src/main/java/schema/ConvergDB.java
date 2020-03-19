d2ca92bc3e4a05567339bad43f23d9fd9f7d4ed4
Kevin-phnew <61772992+kevin-phnew@users.noreply.github.com>
2020-03-19 15:18:48 +0800

    postgre

--- src/main/java/schema/ConvergDB.java
+++ src/main/java/schema/ConvergDB.java
@@ -2,6 +2,7 @@ package schema;
 
 import common.ExtractSchema;
 import metadata.ExtractSchema2ConvergDB;
+import util.FileUtil;
 
 public class ConvergDB {
     public static void main(String[] args) {
@@ -10,12 +11,27 @@ public class ConvergDB {
                             "ConvergDB JDBC schema extractor\n" +
                             "===============================\n");
 
+        if (checkArgs(args)) {
+            return;
+        }
         //Metadata transformation and output
         ExtractSchema extraSchema = new ExtractSchema2ConvergDB();
         extraSchema.outPutSchema();
 
 
 
+    }
+
+    public static boolean checkArgs(String[] args) {
+        boolean status = false;
+        for (String e : args) {
+            if (e.equals("--help")) {
+                status = true;
+                System.out.println(FileUtil.getFile("option"));
+                break;
+            }
+        }
+        return status;
     }
 
 }
